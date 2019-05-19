9e9c3570d3384caca8c88b8166d3de95a10c4767
T_T <tantao700@163.com>
2019-05-19 20:43:31 +0800

    update commit

---
+++ install.sh
@@ -0,0 +1,47 @@
+DIR=$(realpath .)
+echo "install path:$DIR"
+install_ohmyzsh(){
+	echo "install zsh"
+	mv ~/.oh-my-zsh/custom ~/.oh-my-zsh/custom_$(date '+%Y%m%d') 2>&1 >/dev/null
+	ln -s $DIR/oh-my-zsh/ ~/.oh-my-zsh/custom
+}
+
+install_tmux(){
+	echo "install tmux"
+    backup ~/.tmux.conf
+	ln -s $DIR/tmux/tmux.conf ~/.tmux.conf
+}
+
+install_zshrc(){
+	echo "install zshrc"
+    backup ~/.zshrc
+	ln -s $DIR/zsh/zshrc ~/.zshrc
+}
+
+install_vim(){
+	echo "install vim"
+    backup ~/.vim
+	ln -s $DIR/vim ~/.vim
+}
+
+backup(){
+    # soft link
+    if [ -h "$1" ]; then
+        rm "$1"
+    fi
+    # file
+    if [ -e "$1" ]; then
+        echo "backup file/folder to $1_$(date '+%Y%m%d')"
+        mv "$1" "$1_$(date '+%Y%m%d')"
+    fi
+}
+
+if [ -d "~/.oh-my-zsh" ]; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	install_ohmyzsh
+	else
+	install_ohmyzsh		
+fi
+install_zshrc
+install_tmux
+install_vim
